26da3373956deeb56051d182835bb16976b7dab6
Alen Salkovic <salk52@gmail.com>
2024-08-23 15:26:18 +0200

    gitignore fix

---
+++ 02-Comabit-BL/Comabit.BL/Log/Dto/LogItem.cs
@@ -0,0 +1,32 @@
+﻿using Comabit.BL.Company.Dto;
+using Comabit.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comabit.BL.Inquiry.Dto
+{
+    public class LogItem
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public Guid? ProjectId { get; set; }
+
+        public Guid? InquiryId { get; set; }
+
+        public InquiryItem Inquiry { get; set; }
+
+        public Guid? BuyerId { get; set; }
+
+        public Guid? SellerId { get; set; }
+
+        public Guid? OfferId { get; set; }
+
+        public Guid? MatchId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ 03-Comabit-DL/Comabit.DL/Data/Log/Log.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="Log.cs" company="mission-one">
+//      Copyright (c) mission-one. All rights reserved.
+// </copyright>
+
+namespace Comabit.DL.Data.Log
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Log
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public Guid? ProjectId { get; set; }
+
+        public Guid? InquiryId { get; set; }
+
+        public Guid? BuyerId { get; set; }
+
+        public Guid? SellerId { get; set; }
+
+        public Guid? OfferId { get; set; }
+
+        public Guid? MatchId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
